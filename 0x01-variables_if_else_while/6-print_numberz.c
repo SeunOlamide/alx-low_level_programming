11268f0d13a6e5f1c1ed74229c14aecda792389a
Seun Aperolola <Seunaperolola@gmail.com>
2022-09-12 00:33:48 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - assign a random number to the variable n each time it is executed
- * and print the last digit of the number in the variable n
- * Return: Always 0 (Sucess)
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
  */
-
 int main(void)
 {
-	int ch;
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
 
-	for (ch = 48; ch <= 57; ch++)
-	{
-		putchar(ch);
-	}
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
 
-	putchar(10; /* this is an ascii code for new line*/
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
 }
