0a29ea9ddc04ff80cbaa4dc37e270c6c43322061
Seun Aperolola <Seunaperolola@gmail.com>
2022-12-08 10:35:37 -0800

    corrected

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -30,3 +30,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = new;
 		return (new);
 	}
+
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+	new->prev = last;
+
+	return (new);
+}
